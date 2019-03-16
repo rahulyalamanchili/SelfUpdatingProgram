9f4a4cc08e8393a5a53b71d6eb7001efdd803c41
Rahul Yalamanchili <yalaman9@msu.edu>
2019-03-16 12:05:52 -0400

    Update program.py

--- program.py
+++ program.py
@@ -6,7 +6,7 @@ This is the main program that will be deployed to clients
 VERSION = 2.0
 
 def main():
-    print("Running main(), Version: "+str(VERSION))
+    print("program.py::main(), Version: "+str(VERSION))
     
 if __name__ == '__main__':
     main()
