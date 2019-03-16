7b337301e08b8866fc59988495b5d46b47bde894
Rahul Yalamanchili <yalaman9@msu.edu>
2019-03-16 11:48:34 -0400

    Update program.py
    Changed main output

--- program.py
+++ program.py
@@ -3,8 +3,9 @@ This is the main program that will be deployed to clients
 
 @author: Rahul Yalamanchili
 """
+VERSION = 2.0
 
 def main():
-    print("Running main() 2.0")
+    print("Running main()"+str(VERSION))
     
 main()
