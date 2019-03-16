89037dccfa89eef83ce56512af38afec97b7642f
Rahul Yalamanchili <yalaman9@msu.edu>
2019-03-16 12:10:07 -0400

    Update program.py

--- program.py
+++ program.py
@@ -6,6 +6,9 @@ This is the main program that will be deployed to clients
 VERSION = 2.0
 
 def main():
+    """
+    Insert the deliverable code here
+    """
     print("program.py::main(), Version: "+str(VERSION))
     
 if __name__ == '__main__':
